ed6404ce8610f954e057594a9f679798509b5f6c
erwanjouan <erwanjouan@yahoo.fr>
2021-12-04 15:30:01 +0100

    jsonb

---
+++ json/insert_rows.sql
@@ -0,0 +1,10 @@
+INSERT INTO books(client, data) values (
+    'Joe', 
+    '{ "title": "Siddhartha", "author": { "first_name": "Herman", "last_name": "Hesse" } }'
+),(
+    'Jenny', 
+    '{ "title": "Dharma Bums", "author": { "first_name": "Jack", "last_name": "Kerouac" } }'
+),(
+    'Jenny', 
+    '{ "title": "100 años de soledad", "author": { "first_name": "Gabo", "last_name": "Marquéz" } }'
+);
---
+++ json/json.md
@@ -0,0 +1,21 @@
+## Postgres jsonb demo 
+
+Create postgres instance and load json data
+```sh
+make create_instance
+```
+
+Dump table with jsonb 
+```sh
+make dump_json 
+```
+
+Jsonb query
+```sh
+make query_json 
+```
+
+Clean
+```sh
+make drop_instance
+```
